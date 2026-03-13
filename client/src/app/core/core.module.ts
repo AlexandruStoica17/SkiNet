1bd0210fe907cd80f47fdf55a204c26181f63004
Alex <stoica.gh.daniel@student.utcluj.ro>
2026-03-13 22:33:41 +0200

    Sect 10

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AppRoutingModule } from "src/app/app-routing.module";
 
 
 
@@ -9,8 +10,9 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     NavBarComponent
   ],
   imports: [
-    CommonModule
-  ],
+    CommonModule,
+    AppRoutingModule
+],
   exports: [
     NavBarComponent
   ]
